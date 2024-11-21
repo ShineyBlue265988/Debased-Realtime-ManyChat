7f4b3bbda93e16df8ff5bc29f11a60bd244fec00
ShineyBlue265988 <jamesbin265988@gmail.com>
2024-11-21 12:50:53 -0700

    change 16

--- backend/server.js
+++ backend/server.js
@@ -233,7 +233,7 @@ wss.on('connection', (ws) => {
               // console.log("meta", meta);
               // console.log("content", content);
               fullMessages.push({
-                _id: message._id,
+                _id: meta._id,
                 username: message.username,
                 publicKey: message.publicKey,
                 timestamp: message.timestamp,
